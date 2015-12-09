aa205a3e0032730bfecd110f473dd4ac7d15979a
sreniawski <sreniawski@gmail.com>
2015-12-09 09:31:57 -0500

    Update CR_STORAGE_PROVISION_DBCS.sh

--- CR_STORAGE_PROVISION_DBCS.sh
+++ CR_STORAGE_PROVISION_DBCS.sh
@@ -1,17 +1,17 @@
 # !/bin/sh
 #
-curl -v -H GET -H 'X-Storage-User: Storage-metcsgse00382:testuser1' -H 'X-Storage-Pass: password' https://metcsgse00382.storage.oraclecloud.com/v1 &> auth01.txt
+curl -v -H GET -H 'X-Storage-User: Storage-metcsgse00382:cloud.admin' -H 'X-Storage-Pass: latinCzech+1' https://metcsgse00382.storage.oraclecloud.com/auth/v1.0 &> auth01.txt
 
 awk '/X-Auth-Token:/{b=$3}END{print b}' auth01.txt
 
 auth01="'X-Auth_Token: $(awk '/X-Auth-Token:/{b=$3}END{print b}' auth01.txt)'"
 
 echo $auth01
-echo "curl -i  https://metcsgse00382.storage.oraclecloud.com/v1/Storage-metcsgse00382/DBCScontainer -X PUT -H ‘Content-Length: 0’ -H ${auth01}" > ./list1.sh
+echo "curl -i  https://storage.us2.oraclecloud.com/v1/Storage-metcsgse00382/DBCScontainer -X PUT -H ‘Content-Length: 0’ -H ${auth01}" > ./list1.sh
 
-echo "curl -i  https://metcsgse00382.storage.oraclecloud.com/v1/Storage-metcsgse00382/JCScontainer -X PUT -H ‘Content-Length: 0’ -H ${auth01}" >> ./list1.sh
+echo "curl -i  https://storage.us2.oraclecloud.com/v1/Storage-metcsgse00382/JCScontainer -X PUT -H ‘Content-Length: 0’ -H ${auth01}" >> ./list1.sh
 
-echo "curl -i  https://metcsgse00382.storage.oraclecloud.com/v1/Storage-metcsgse00382 -X GET -H ${auth01}" >> ./list1.sh
+echo "curl -i  https://storage.us2.oraclecloud.com/v1/Storage-metcsgse00382 -X GET -H ${auth01}" >> ./list1.sh
 
 
 chmod 744 ./list1.sh
@@ -20,4 +20,4 @@ chmod 744 ./list1.sh
 sleep 100
 
 
-curl -i -X POST -u oracle:welcome1 -d @DBCS_Provision.json -H "X-ID-TENANT-NAME: metcsgse00382" -H "Content-Type:application/json" https://dbaas.oraclecloud.com/jaas/db/api/v1.1/instances/metcsgse00382
+curl -i -X POST -u cloud.admin:latinCzech+1 -d @DBCS_Provision.json -H "X-ID-TENANT-NAME: metcsgse00382" -H "Content-Type:application/json" https://dbaas.oraclecloud.com/jaas/db/api/v1.1/instances/metcsgse00382
